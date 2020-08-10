d60e8189ba13aef409cac3314a27e027a5b95d59
wyattharrell <wyattaharrell@gmail.com>
2020-08-10 17:55:44 -0400

    File reorganization

---
+++ labs-ios-starter/View Controllers/Dashboard/DashboardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardViewController.swift
+//  labs-ios-starter
+//
+//  Created by Wyatt Harrell on 8/10/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    // MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
+
+extension DashboardViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
