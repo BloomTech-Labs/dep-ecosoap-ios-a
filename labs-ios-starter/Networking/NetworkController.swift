7908bf748db8cabde3366cf34d7c23bd7b548437
Hector <hectoremil21@hotmail.com>
2020-08-13 00:57:49 -0400

    BackendController.queryAPI successfully receives data back from API.

--- labs-ios-starter/Networking/NetworkController.swift
+++ labs-ios-starter/Networking/NetworkController.swift
@@ -9,34 +9,34 @@
 import Foundation
 
 class BackendController {
-    private let apiURL: URL = URL(string: "ecosoap-placeholder.herokuapp.com/graphiql")!
+    private let apiURL: URL = URL(string: "https://ecosoap-placeholder.herokuapp.com/graphql")!
     
     private var encoder = JSONEncoder()
     private var decoder = JSONDecoder()
     
-    func queryAPI(completion: @escaping (String, Any?) -> Void) {
+    func queryAPI(query: String, completion: @escaping (String, Any?, Error?) -> Void) {
         var request = URLRequest(url: apiURL)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        request.httpBody = try! JSONSerialization.data(withJSONObject: ["query":Queries.statsById], options: [])
+        request.httpBody = try! JSONSerialization.data(withJSONObject: ["query":query], options: [])
         
         URLSession.shared.dataTask(with: request) { data, _, error in
             if let _ = error {
-                completion("No bueno", nil)
+                completion("No bueno", nil, error)
                 return
             }
             
             guard let data = data else {
-                completion("No Data", nil)
+                completion("No Data", nil, nil)
                 return
             }
             
             do {
                 let dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-                completion("We got stuff", dict)
+                completion("We got stuff", dict, nil)
             } catch let error {
-                completion("No cigar", nil)
+                completion("No cigar", nil, error)
             }
         }.resume()
         
