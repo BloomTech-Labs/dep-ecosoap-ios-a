0c14e4646187a97bb84977c378bcacdda196e3b3
wyattharrell <wyattaharrell@gmail.com>
2020-08-18 19:10:47 -0400

    Customized custom cell

--- labs-ios-starter/Views/Impact Statistics/ImpactStatisticsIndividualCollectionViewCell.swift
+++ labs-ios-starter/Views/Impact Statistics/ImpactStatisticsIndividualCollectionViewCell.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class ImpactStatisticsIndividualCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - View Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+    
+    // MARK: - Private Methods
+    private func setupViews() {
+        self.layer.cornerRadius = 8.0
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.backgroundColor = UIColor(named: "Panel System Background")?.cgColor
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOpacity = 0.25
+        self.layer.masksToBounds = false
+    }
 }
