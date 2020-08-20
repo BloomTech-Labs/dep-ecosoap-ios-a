1366901be49aba01a8e668e575815bf75e193112
Hector <hectoremil21@hotmail.com>
2020-08-19 23:28:10 -0400

    Create Enums.swift file. Contains all Schema enums.

---
+++ labs-ios-starter/Models/Enums.swift
@@ -0,0 +1,33 @@
+//
+//  Enums.swift
+//  labs-ios-starter
+//
+//  Created by Karen Rodriguez on 8/19/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+enum PropertyType {
+    case BED_AND_BREAKFAST
+    case GUESTHOUSE
+    case HOTEL
+    case OTHER
+}
+
+
+enum HospitalityService {
+    case BOTTLES
+    case LINENS
+    case OTHER
+    case PAPER
+    case SOAP
+}
+
+enum CollectionType {
+    case COURIER_CONSOLIDATED
+    case COURIER_DIRECT
+    case GENERATED_LABEL
+    case LOCAL
+    case OTHER
+}
