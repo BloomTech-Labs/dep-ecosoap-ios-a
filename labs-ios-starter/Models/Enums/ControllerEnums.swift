a63d8f59467954e1d084e9f7ff52cb6c1334aab5
Hector <hectoremil21@hotmail.com>
2020-08-26 00:53:29 -0400

    Create ControllerEnums. Implement ResponseModel enum.
    ResponseModel enum will be used for a larger scale refactoring later on.

---
+++ labs-ios-starter/Models/Enums/ControllerEnums.swift
@@ -0,0 +1,20 @@
+//
+//  ControllerEnums.swift
+//  labs-ios-starter
+//
+//  Created by Karen Rodriguez on 8/26/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+enum ResponseModel: String {
+    case user
+    case property
+    case properties
+    case impactStats
+    case hub
+    case pickups
+    case payment
+    case payments
+}
