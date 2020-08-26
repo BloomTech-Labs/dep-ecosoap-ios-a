ad43f961c66ad371aec0ab48b84e27db4f56524b
Hector <hectoremil21@hotmail.com>
2020-08-25 20:54:15 -0400

    Create Mutator.swift file.

---
+++ labs-ios-starter/Networking/Mutator.swift
@@ -0,0 +1,13 @@
+//
+//  Mutator.swift
+//  labs-ios-starter
+//
+//  Created by Karen Rodriguez on 8/25/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+class Mutator {
+    
+}
