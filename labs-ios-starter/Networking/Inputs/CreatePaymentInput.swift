d8524a66aa496a0bc40e75ef7a8c19bee829c975
Hector <hectoremil21@hotmail.com>
2020-08-26 19:13:46 -0400

    Create CreatePaymentInput, conform to Input protocol.

---
+++ labs-ios-starter/Networking/Inputs/CreatePaymentInput.swift
@@ -0,0 +1,32 @@
+//
+//  CreatePaymentInput.swift
+//  labs-ios-starter
+//
+//  Created by Karen Rodriguez on 8/26/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+class CreatePaymentInput: Input {
+
+    let amountPaid: Int
+    let date: Date
+    let paymentMethod: String
+    let hospitalityContractId: String
+
+    var formatted: String {
+        let string = """
+
+    """
+        return string
+    }
+
+    init(amountPaid: Int, date: Date, paymentMethod: PaymentMethod, hospitalityContractId: String) {
+        self.amountPaid = amountPaid
+        self.date = date
+        self.paymentMethod = paymentMethod.rawValue
+        self.hospitalityContractId = hospitalityContractId
+    }
+
+}
