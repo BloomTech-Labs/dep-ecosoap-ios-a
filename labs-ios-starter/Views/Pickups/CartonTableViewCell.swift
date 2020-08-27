4122cdb764c57656c01f790d88b11feb9d6b951f
wyattharrell <wyattaharrell@gmail.com>
2020-08-27 12:41:42 -0400

    Create tableviewcell

---
+++ labs-ios-starter/Views/Pickups/CartonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartonTableViewCell.swift
+//  labs-ios-starter
+//
+//  Created by Wyatt Harrell on 8/27/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class CartonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
