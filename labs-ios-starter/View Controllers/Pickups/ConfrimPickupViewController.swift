2047914a928d2e34daf914cb30220f0819f0876d
wyattharrell <wyattaharrell@gmail.com>
2020-08-31 18:52:59 -0400

    Setup marks for last commit on this branch

--- labs-ios-starter/View Controllers/Pickups/ConfrimPickupViewController.swift
+++ labs-ios-starter/View Controllers/Pickups/ConfrimPickupViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ConfrimPickupViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func setupViews() {
+        
     }
-    */
+    
+    private func updateViews() {
+        
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    // MARK: - IBActions
+
 
 }
