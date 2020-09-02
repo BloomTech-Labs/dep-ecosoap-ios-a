def5d514347fc284a27cedd10d9871ca6d517e10
wyattharrell <wyattaharrell@gmail.com>
2020-09-02 14:31:44 -0400

    ProfileViewController conforms to delegate and data source

--- labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
+++ labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
@@ -38,3 +38,15 @@ class ProfileViewController: UIViewController {
 
 
 }
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+}
