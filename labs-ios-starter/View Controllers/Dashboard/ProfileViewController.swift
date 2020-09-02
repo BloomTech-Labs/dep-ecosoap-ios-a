0e90aa72eb55739d35b347cb4f12b360d090af27
wyattharrell <wyattaharrell@gmail.com>
2020-09-02 14:28:07 -0400

    Created IBOutlets

--- labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
+++ labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func setupViews() {
+        
     }
-    */
+    
+    private func updateViews() {
+        
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    // MARK: - IBActions
+
 
 }
