ef07d59ebb0337be5a381e9c01126f5d1a1b6ac5
wyattharrell <wyattaharrell@gmail.com>
2020-09-02 14:32:08 -0400

    Properties conforms to delegate and data source

--- labs-ios-starter/View Controllers/Dashboard/PropertiesViewController.swift
+++ labs-ios-starter/View Controllers/Dashboard/PropertiesViewController.swift
@@ -38,3 +38,15 @@ class PropertiesViewController: UIViewController {
 
 
 }
+
+extension PropertiesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+}
