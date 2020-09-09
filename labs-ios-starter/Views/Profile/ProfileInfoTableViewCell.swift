ca6712868b7a559a1415fc0a4a13f0f8ad79fcd2
wyattharrell <wyattaharrell@gmail.com>
2020-09-09 00:47:22 -0400

    Created Profile Info Table View Cell

---
+++ labs-ios-starter/Views/Profile/ProfileInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileInfoTableViewCell.swift
+//  labs-ios-starter
+//
+//  Created by Wyatt Harrell on 9/9/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ProfileInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
