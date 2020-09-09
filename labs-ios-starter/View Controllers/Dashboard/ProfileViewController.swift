4d64a86ec343278a9cb68893e18d123659977d40
wyattharrell <wyattaharrell@gmail.com>
2020-09-09 01:10:38 -0400

    Setting up Profile tableview data

--- labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
+++ labs-ios-starter/View Controllers/Dashboard/ProfileViewController.swift
@@ -14,38 +14,31 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
+    private let titleLabels = ["Name", "Company", "Email", "Phone", "Skype", "Address"]
+    private let iconImageViews = [UIImage(systemName: "person.crop.square.fill"), UIImage(systemName: "briefcase.fill"), UIImage(systemName: "envelope.fill"), UIImage(systemName: "phone.fill"), UIImage(systemName: "desktopcomputer"), UIImage(systemName: "house.fill")]
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-    }
-    
-    // MARK: - Private Methods
-    private func setupViews() {
-        
-    }
-    
-    private func updateViews() {
-        
     }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
-    
-    // MARK: - IBActions
-
-
 }
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return titleLabels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileInfoCell", for: indexPath) as? ProfileInfoTableViewCell else { return UITableViewCell() }
+        
+        cell.titleLabel.text = titleLabels[indexPath.row]
+        cell.iconImageView.image = iconImageViews[indexPath.row]
+        cell.iconImageView.tintColor = iconTints[indexPath.row]
+        cell.descriptionTextField.text = placeholderData[indexPath.row]
         
         return cell
     }
