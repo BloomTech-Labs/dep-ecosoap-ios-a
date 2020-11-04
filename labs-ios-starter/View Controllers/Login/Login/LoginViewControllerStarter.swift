80b909cac73a8da60b51577b5baecf1bbcd9c37a
nonye <nnezekwo@gmail.com>
2020-11-03 20:03:58 -0800

    UI-updated LoginViewController file and LaunchScreen storyboard

--- labs-ios-starter/View Controllers/Login/Login/LoginViewControllerStarter.swift
+++ labs-ios-starter/View Controllers/Login/Login/LoginViewControllerStarter.swift
@@ -37,7 +37,8 @@ class LoginViewControllerStarter: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.boldSystemFont(ofSize: 25.0)
         label.numberOfLines = 0
-        label.text = "EcoSoap-Bank"
+        label.text = "eco-soap bank"
+        label.textColor = .white
         label.textAlignment = .center
         return label
     }()
@@ -57,7 +58,7 @@ class LoginViewControllerStarter: UIViewController {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Register", for: .normal)
-        button.setTitleColor(UIColor(named: "ESB Blue"), for: .normal)
+        button.setTitleColor(UIColor(named: "ESB Green"), for: .normal)
         button.heightAnchor.constraint(equalToConstant: 30).isActive = true
         return button
     }()
@@ -66,52 +67,50 @@ class LoginViewControllerStarter: UIViewController {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Forgot Password?", for: .normal)
-        button.setTitleColor(UIColor(named: "ESB Blue"), for: .normal)
+        button.setTitleColor(UIColor(named: "ESB Green"), for: .normal)
         button.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
         return button
     }()
-    
+    ///update
     private lazy var loginButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Login", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = UIColor(named: "ESB Green")
+        button.setTitle("Log in", for: .normal)
+        button.setTitleColor(UIColor(named: "ESB Green"), for: .normal)
+        button.backgroundColor = UIColor.white
         button.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
         button.layer.cornerRadius = 8
         button.addTarget(self, action:#selector(self.login), for: .touchUpInside)
         return button
     }()
-    
+    //updated
     private lazy var usernameTextField: UITextField = {
         let textfield = UITextField()
+        let borderColor = UIColor.white
+        
         textfield.translatesAutoresizingMaskIntoConstraints = false
-        textfield.borderStyle = .none
+        textfield.layer.borderColor = borderColor.cgColor
+        textfield.layer.borderWidth = 1.0
         textfield.textContentType = .username
         textfield.clearButtonMode = .whileEditing
-        textfield.placeholder = "  Username"
         textfield.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
         textfield.layer.cornerRadius = 8
-        let imageView = UIImageView(image: UIImage(systemName: "person.fill"))
-        imageView.tintColor = .lightGray
-        textfield.leftView = imageView
-        textfield.leftViewMode = .always
+        textfield.attributedPlaceholder = NSAttributedString(string: "  Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         return textfield
     }()
-    
+    //updated
     private lazy var passwordTextField: UITextField = {
         let textfield = UITextField()
+        let borderColor = UIColor.white
+        
         textfield.translatesAutoresizingMaskIntoConstraints = false
-        textfield.borderStyle = .none
+        textfield.layer.borderColor = borderColor.cgColor
+        textfield.layer.borderWidth = 1.0
         textfield.textContentType = .password
         textfield.clearButtonMode = .whileEditing
-        textfield.placeholder = "  Password"
         textfield.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
         textfield.layer.cornerRadius = 8
-        let imageView = UIImageView(image: UIImage(systemName: "lock.fill"))
-        imageView.tintColor = .lightGray
-        textfield.leftView = imageView
-        textfield.leftViewMode = .always
+        textfield.attributedPlaceholder = NSAttributedString(string: "  Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         return textfield
     }()
     
@@ -124,7 +123,7 @@ class LoginViewControllerStarter: UIViewController {
         stackView.spacing = 8
         return stackView
     }()
-    
+    //updated
     private lazy var textfieldStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -134,7 +133,7 @@ class LoginViewControllerStarter: UIViewController {
         stackView.spacing = 8
         return stackView
     }()
-    
+    //updated
     private lazy var usernameStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -144,7 +143,7 @@ class LoginViewControllerStarter: UIViewController {
         stackView.spacing = 0
         return stackView
     }()
-    
+    //updated
     private lazy var passwordStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -154,37 +153,28 @@ class LoginViewControllerStarter: UIViewController {
         stackView.spacing = 0
         return stackView
     }()
-    
+    //updated
     private lazy var usernameView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
-        view.backgroundColor = UIColor(named: "ESB Blue")
         return view
     }()
-    
+    //updated
     private lazy var passwordView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
-        view.backgroundColor = UIColor(named: "ESB Blue")
         return view
     }()
-    
+    //updated
     private lazy var panelView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.layer.cornerRadius = 8
-        view.backgroundColor = UIColor(named: "Panel System Background")
         return view
     }()
     
-    private lazy var imageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "ESB Logo"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
     
     // MARK: - Actions
     @objc func login() {
@@ -195,29 +185,24 @@ class LoginViewControllerStarter: UIViewController {
     private func setupViews() {
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
-        view.backgroundColor = UIColor(named: "ESB System Background")
+        view.backgroundColor = UIColor(named: "ESB Green")
         
-        // ESB Logo
-        view.addSubview(imageView)
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
-        imageView.heightAnchor.constraint(lessThanOrEqualTo: view.heightAnchor, multiplier: 0.20).isActive = true
-        
+        //updated
         // Labels
         infoLabelStackView.addArrangedSubview(titleLabel)
-        infoLabelStackView.addArrangedSubview(descriptionLabel)
         view.addSubview(infoLabelStackView)
+        infoLabelStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80.0).isActive = true
         infoLabelStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
         infoLabelStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        infoLabelStackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
-        
+
+        //updated
         // Panel View
         view.addSubview(panelView)
         panelView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         panelView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         panelView.topAnchor.constraint(equalTo: infoLabelStackView.bottomAnchor, constant: 20).isActive = true
-
+        
+        //updated
         // Textfields
         usernameStackView.addArrangedSubview(usernameTextField)
         usernameStackView.addArrangedSubview(usernameView)
@@ -230,6 +215,7 @@ class LoginViewControllerStarter: UIViewController {
         textfieldStackView.trailingAnchor.constraint(equalTo: panelView.trailingAnchor, constant: -20).isActive = true
         textfieldStackView.topAnchor.constraint(equalTo: panelView.topAnchor, constant: 20).isActive = true
         
+        //updated
         // Login Button
         panelView.addSubview(loginButton)
         loginButton.leadingAnchor.constraint(equalTo: panelView.leadingAnchor, constant: 20).isActive = true
@@ -237,6 +223,7 @@ class LoginViewControllerStarter: UIViewController {
         loginButton.bottomAnchor.constraint(equalTo: panelView.bottomAnchor, constant: -20).isActive = true
         loginButton.topAnchor.constraint(equalTo: textfieldStackView.bottomAnchor, constant: 20).isActive = true
         
+        //updated
         // Forgot Password Button
         view.addSubview(forgotPasswordButton)
         forgotPasswordButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
