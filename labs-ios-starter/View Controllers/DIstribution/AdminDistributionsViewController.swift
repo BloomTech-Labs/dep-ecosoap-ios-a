1016e770145510e10df40893eb1e143c0cda87c4
Brian Vilchez <bfvilchez@icloud.com>
2020-11-04 16:27:56 -0700

    Distribution
    Storyboard & files connected with IBActions &IBoutlets

---
+++ labs-ios-starter/View Controllers/DIstribution/AdminDistributionsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AdminDistributionsViewController.swift
+//  labs-ios-starter
+//
+//  Created by Brian Vilchez on 11/4/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class AdminDistributionsViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var newButton: UIButton!
+    @IBOutlet weak var distributionTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNewButton()
+        configureTableView()
+    }
+    
+
+    // MARK: - Helper Methods
+    private func configureNewButton() {
+        newButton.layer.cornerRadius = 5
+    }
+    
+    private func configureTableView() {
+        distributionTableView.delegate = self
+        distributionTableView.dataSource = self
+        
+    }
+    
+    
+    // MARK: - IBActions
+    @IBAction func newTapped(_ sender: UIButton) {
+        
+        // TODO:
+    }
+    
+    @IBAction func filtersTapped(_ sender: UIButton) {
+        
+        // TODO:
+    }
+}
+
+// MARK: - TableView DataSource & Delegate
+extension AdminDistributionsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = distributionTableView.dequeueReusableCell(withIdentifier: "DistributionCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
