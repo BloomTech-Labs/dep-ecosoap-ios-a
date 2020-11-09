94f8686147ba7096b3b517180f3cc5d55e90b880
Cody Morley <codymorleyinc@gmail.com>
2020-11-08 20:40:38 -0800

    Added UpdateCorporateSponsorInput.swift
    Added a file for the graphQL input class handling network calls to update CorporateSponsor objects to Networking folder

---
+++ labs-ios-starter/Networking/Inputs/UpdateCorporateSponsorInput.swift
@@ -0,0 +1,9 @@
+//
+//  UpdateCorporateSponsorInput.swift
+//  labs-ios-starter
+//
+//  Created by Cody Morley on 11/8/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
