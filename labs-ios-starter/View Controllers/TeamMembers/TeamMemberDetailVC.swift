99e9a62d288df7845dd007992b3ad5cbf617febf
Dahna <dbuenrostro09@gmail.com>
2020-11-09 12:48:21 -0500

    Team Member creating/updating UI

---
+++ labs-ios-starter/View Controllers/TeamMembers/TeamMemberDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  TeamMemberDetailVC.swift
+//  labs-ios-starter
+//
+//  Created by Dahna on 11/9/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class TeamMemberDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
