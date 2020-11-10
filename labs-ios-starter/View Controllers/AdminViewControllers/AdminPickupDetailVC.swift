6c8b90cdeed070bf39c41b9da13a894f258fad21
nonye <nnezekwo@gmail.com>
2020-11-10 12:57:26 -0800

    Pickup corresponding files created

---
+++ labs-ios-starter/View Controllers/AdminViewControllers/AdminPickupDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  AdminPickupDetailVC.swift
+//  labs-ios-starter
+//
+//  Created by Nonye on 11/10/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class AdminPickupDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
