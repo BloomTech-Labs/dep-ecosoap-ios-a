e71ca9ba2ff2d5b72f7064cbb3fb951bfcfbe9b9
Stephanie <user.name>
2020-11-10 14:26:44 -0800

    Admin Dashboard

--- labs-ios-starter/View Controllers/AdminViewControllers/AdminMainViewController.swift
+++ labs-ios-starter/View Controllers/AdminViewControllers/AdminMainViewController.swift
@@ -8,12 +8,9 @@
 
 import UIKit
 
-//TODO: This is the APEX location where all the segues to sub-sections are stored. Create buttons and segue logic to handle transition to sub-sections.
-//TODO: MVP: We need the segue to AdminProfileVC -> AdminDetailVC (to edit user info)
-
 class AdminMainViewController: UIViewController {
     
-    @IBOutlet var adminDashboardButtons: [UIButton]!
+    @IBOutlet var adminDashboardButtons: [DashboardButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,79 +19,34 @@ class AdminMainViewController: UIViewController {
     
     @IBAction func productionReportButtonTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "", sender: nil)
-        
-        // Creates illusion of animation when button is tapped
-        sender.alpha = 0.5
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1.0
-        }
     }
     
     @IBAction func hubProfilesButtonTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "", sender: nil)
-        
-        // Creates illusion of animation when button is tapped
-        sender.alpha = 0.5
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1.0
-        }
     }
     
     @IBAction func teamMembersButtonTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "", sender: nil)
-        
-        // Creates illusion of animation when button is tapped
-        sender.alpha = 0.5
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1.0
-        }
     }
     
     @IBAction func pickUpsButtonTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "", sender: nil)
-        
-        // Creates illusion of animation when button is tapped
-        sender.alpha = 0.5
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1.0
-        }
     }
     
     @IBAction func profileButtonTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "", sender: nil)
-        
-        // Creates illusion of animation when button is tapped
-        sender.alpha = 0.5
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.alpha = 1.0
-        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
     
     // MARK: - Button Setup -
     func buttonSetup() {
         for button in adminDashboardButtons {
             button.setWidth(width: 190)
             button.setHeight(height: 120)
-            button.backgroundColor = UIColor(red: 18/255, green: 89/255, blue: 11/255, alpha: 1.0)
             button.layer.cornerRadius = 12.0
             button.layer.borderWidth = 1.0
             button.layer.borderColor = UIColor.white.cgColor
-            button.setTitleColor(UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1.0), for: .normal)
-            button.titleLabel?.textColor = .black
+            button.tintColor = .white
             button.titleLabel?.font = UIFont(name: "Futura", size: 20)
             
             // Shadow
